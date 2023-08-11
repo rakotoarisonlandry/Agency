5d4bafd709962691b9eee8a3f928a352b625dcba
rakotoarisonlandry <rakotorisonlandry@gmail.com>
2023-08-11 11:46:31 +0300

    new commit

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,7 +1,8 @@
+import { VpnLock } from "@mui/icons-material";
 import { Stack } from "@mui/material";
 import React from "react";
 import IllustrationForm from "../assets/image/Home_C.gif";
-
+import Validation from "./formValidation";
 function Form() {
   return (
     <React.Fragment>
@@ -20,6 +21,7 @@ function Form() {
             height="500px"
           />
         </div>
+        <Validation/>
       </Stack>
     </React.Fragment>
   );
