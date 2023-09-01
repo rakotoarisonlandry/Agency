f74d74fb32bd753f5d73a2423b7dd6e7329a886a
rakotoarisonlandry <rakotorisonlandry@gmail.com>
2023-09-01 15:00:06 +0300

    ne change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         className="min-h-screen bg-[#f7f7f7]"
         style={{
           backgroundImage: `url(${illu})`,
-          backgroundPosition: "0 -240px",
+          backgroundPosition: "0 -300px",
           backgroundSize: "100%",
           backgroundRepeat: "no-repeat",
           
