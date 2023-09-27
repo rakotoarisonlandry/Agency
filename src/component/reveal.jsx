0c69a1fccd42043207438366d127a9bafb1417c3
rakotoarisonlandry <rakotorisonlandry@gmail.com>
2023-09-27 10:48:01 +0300

    animation while in view

--- src/component/reveal.jsx
+++ src/component/reveal.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useRef } from "react";
-import { animate, motion, useInView } from "framer-motion";
+import { animate, motion, useAnimation, useInView } from "framer-motion";
 
 const Reveal = ({ children, width = "fit-content" }) => {
   const ref = useRef(null);
   const isInView =useInView(ref, {once :true})
+  const mainControls =useAnimation();
+  useEffect(()=>{
+    if(isInView){
+        mainControls.start("visible")
+    }
+  })
     return (
-    <motion.div
+    <motion.div ref={ref}
         variants={{
             hidden:{opacity:0,y:75},
             visible:{opacity:1,y:0}
         }}  
         initial="hidden"
-        animate="visible"
+        animate={mainControls}
+        transition={{duration:0.5 ,delay :0.25}}
     >   
       {children}
     </motion.div>
